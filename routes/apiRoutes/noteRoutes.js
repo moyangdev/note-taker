a3459b495876a512214e280ccb422978d5fcb632
Mo Yang <moyang313@gmail.com>
2022-05-01 12:16:55 -0400

    add route to handle delete requests

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { createNewNote } = require('../../lib/notes');
+const { createNewNote, deleteNote } = require('../../lib/notes');
 const { notes } = require('../../db/db');
 
 router.get('/notes', (req, res) => {
@@ -10,10 +10,14 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
      // set id based on what the next index of the array will be
     req.body.id = notes.length.toString();
-
     const note = createNewNote(req.body, notes);
     res.json(note);
     }
 );
+
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(notes, req.params.id);
+    res.json(notes);
+})
 
 module.exports = router;
